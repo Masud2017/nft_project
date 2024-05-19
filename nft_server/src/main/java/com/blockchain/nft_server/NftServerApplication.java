a3b8030a0b6e60cc7380ae9aaf2c6906f4d472b1
Masud2017 <msmasud578@gmail.com>
2024-05-19 14:43:25 +0600

    init commit

---
+++ nft_server/src/main/java/com/blockchain/nft_server/NftServerApplication.java
@@ -0,0 +1,13 @@
+package com.blockchain.nft_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NftServerApplication.class, args);
+    }
+
+}
---
+++ nft_server/src/test/java/com/blockchain/nft_server/NftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockchain.nft_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
