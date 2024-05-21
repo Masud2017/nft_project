76971fc24cd52e8dcdaa7c8f37a4026a61ae4664
Masud2017 <msmasud578@gmail.com>
2024-05-21 11:56:37 +0600

    updated the server

--- nft_server/src/test/java/com/blockchain/nft_server/NftServerApplicationTests.java
+++ nft_server/src/test/java/com/blockchain/nft_server/NftServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NftServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
